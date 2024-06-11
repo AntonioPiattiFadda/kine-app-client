0b410b9850190925d2a1de9482e3e9aa60e24e90
AntonioPiattiFadda <antonio.piattifadda@gmail.com>
2024-06-11 16:11:27 -0300

    modo prueba con link

--- src/components/client/startOverlay/StartOverlay.tsx
+++ src/components/client/startOverlay/StartOverlay.tsx
@@ -1,19 +1,25 @@
+import { Link } from 'react-router-dom';
 import PlanStartImage from '../../../assets/planStartImage.jpg';
 import './StartOverlay.css';
 
 const StartOverlay = () => {
-  const handleOverlayClick = () => {
-    const planOverlay = document.getElementById('planOverlay');
-    planOverlay?.classList.toggle('planOverlayClosed');
-    window.location.href =
-      '/cli/patient?uidpat=yBoieYZYmx00aWVaJAHn&&uidprof=auc92632FbRbnVRF85bRW77i7a83';
-  };
+  // const handleOverlayClick = () => {
+  //   const planOverlay = document.getElementById('planOverlay');
+  //   planOverlay?.classList.toggle('planOverlayClosed');
+  // };
   return (
     <div className="planOverlay" id="planOverlay">
       <img src={PlanStartImage} alt="asdfads" />
       <div>
         <p>Desbloquea tu potencial</p>
-        <button onClick={handleOverlayClick}>{'Comienza tu Plan'}</button>
+        <Link
+          to={
+            '/cli/patient?uidpat=yBoieYZYmx00aWVaJAHn&&uidprof=auc92632FbRbnVRF85bRW77i7a83'
+          }
+        >
+          {' '}
+          <button>{'Comienza tu Plan'}</button>
+        </Link>
       </div>
     </div>
   );
